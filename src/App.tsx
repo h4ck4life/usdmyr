9355e70dec3e99e10fb44869ceb12f2939e4469f
Alif <alifaziz@gmail.com>
2021-06-10 23:04:11 +0800

    fix warnings

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Converter from "./components/Converter";
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function App() {
   return (
     <div className="container mx-auto">
