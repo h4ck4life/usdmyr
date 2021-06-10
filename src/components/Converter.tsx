8dc7d7946cf422eed57de9baccc02f076ad9319e
Alif <alifaziz@gmail.com>
2021-06-10 22:47:08 +0800

    clean up some ui + add google fonts

--- src/components/Converter.tsx
+++ src/components/Converter.tsx
@@ -1,4 +1,5 @@
 import React, { ChangeEvent, useEffect, useState } from "react";
+import dayjs from "dayjs";
 //import { BsArrowLeftRight } from "react-icons/bs";
 
 let typingTimer: any = null;
@@ -11,11 +12,11 @@ export const Converter = () => {
 
   const getMyrToUsd = (inputEl?: ChangeEvent<HTMLInputElement>) => {
     window.clearTimeout(typingTimer);
-    setLoading(true);
     const amount = inputEl ? inputEl.target.value : myrAmount;
     setMyrAmount(amount);
     typingTimer = setTimeout(async () => {
       try {
+        setLoading(true);
         const response = await fetch(
           `https://api.exchangerate.host/latest?base=MYR&amount=${
             amount !== "" ? amount : "1"
@@ -30,16 +31,16 @@ export const Converter = () => {
       } finally {
         setLoading(false);
       }
-    }, 800);
+    }, 1000);
   };
 
   const getUsdToMyr = (inputEl?: ChangeEvent<HTMLInputElement>) => {
     window.clearTimeout(typingTimer);
-    setLoading(true);
     const amount = inputEl ? inputEl.target.value : usdAmount;
     setUsdAmount(amount);
     typingTimer = setTimeout(async () => {
       try {
+        setLoading(true);
         const response = await fetch(
           `https://api.exchangerate.host/latest?base=USD&amount=${
             amount !== "" ? amount : "1"
@@ -64,12 +65,12 @@ export const Converter = () => {
   }, []);
 
   return (
-    <div className="mt-5">
-      <div className="flex justify-center flex-col text-center text-5xl text-blue-100 font-bold">
-        <div className="flex-col flex-wrap">
-          <span className="mr-2">USD</span>
+    <div className="flex flex-col justify-center text-center">
+      <div className="flex justify-center flex-col text-center text-5xl text-blue-100">
+        <div className="mt-7 flex-row md:flex-col flex-wrap">
+          <span className="lg:mr-4 md:mr-0 text-blue-400">USD</span>
           <input
-            className={`my-2 w-full text-6xl bg-blue-800 p-3 text-center focus:outline-none rounded-md font-bold ${
+            className={`my-2 lg:w-auto w-full text-7xl bg-blue-900 p-3 text-center lg:text-left lg:pl-4 focus:outline-none ${
               isLoading ? "text-blue-600 animate-pulse" : "text-blue-100"
             }`}
             type="number"
@@ -78,10 +79,10 @@ export const Converter = () => {
             value={usdAmount}
           />
         </div>
-        <div className="flex-col flex-wrap">
-          <span className="mr-2">MYR</span>
+        <div className="mt-7 flex-col flex-wrap">
+          <span className="lg:mr-4 md:mr-0 text-blue-400">MYR</span>
           <input
-            className={`my-2 w-full text-6xl bg-blue-800 p-3 text-center focus:outline-none rounded-md font-bold ${
+            className={`my-2 lg:w-auto w-full text-7xl bg-blue-900 p-3 text-center lg:text-left lg:pl-4 focus:outline-none ${
               isLoading ? "text-blue-600 animate-pulse" : "text-blue-100"
             }`}
             type="number"
@@ -90,10 +91,11 @@ export const Converter = () => {
             onChange={getMyrToUsd}
           />
         </div>
-         <div className="flex-col flex-wrap text-xs text-blue-500">
-            {updatedDate !== "" && updatedDate}
-         </div>   
       </div>
+      {/* <div className="mt-4 flex-col flex-wrap text-xs text-blue-500">
+        {updatedDate !== "" &&
+          dayjs(updatedDate, "YYYY-MM-DD").format("D MMM YYYY")}
+      </div> */}
     </div>
   );
 };
